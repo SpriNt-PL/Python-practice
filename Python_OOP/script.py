b0039979cca959e62a9f5f73cd58dff84fd5849f
Kuba <kuba@example.com>
2026-03-08 14:04:00 +0100

    __str__ method adjusted to inherited class

--- Python_OOP/script.py
+++ Python_OOP/script.py
@@ -1,20 +1,24 @@
 class Item:
     def __init__(self, name, weight, price):
-        self.__name = name
-        self.__weight = weight
-        self.__price = price
+        self._name = name
+        self._weight = weight
+        self._price = price
 
     def get_name(self):
-        return self.__name
+        return self._name
     
     def __str__(self):
-        return f"{self.__name} ({self.__weight}kg) - {self.__price} gold"
+        return f"{self._name} ({self._weight}kg) - {self._price} gold"
 
 class Weapon(Item):
-    def __init__(self, name, weight, price):
+    def __init__(self, name, weight, price, damage):
         super().__init__(name, weight, price)
+        self._damage = damage
 
-i = Weapon("Sword", 7, 53)
+    def __str__(self):
+        return f"{self._name} ({self._weight}kg) - {self._price} gold, {self._damage} damage"
+
+i = Weapon("Sword", 7, 53, 11)
 
 print(i)
 
